35a6c868b5643a670017bb103383cf53968b4e26
Hichem <hichem.djadoudi@gmail.com>
2021-12-21 16:45:12 +0400

    varChange

--- command_line_app/src/main.rs
+++ command_line_app/src/main.rs
@@ -11,6 +11,6 @@ fn main() {
     println!("Weight on Mars: {}kg", mars_weight);
 }
 
-fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) * 3.711
+fn calculate_weight_on_mars(_weight: f32) -> f32 {
+    (_weight / 9.81) * 3.711
 }
---
+++ command_line_app/target/debug/deps/command_line_app.d
@@ -0,0 +1,5 @@
+C:\Users\M.Djadoud\Desktop\Rust_Project_PC\command_line_app\target\debug\deps\command_line_app.exe: src\main.rs
+
+C:\Users\M.Djadoud\Desktop\Rust_Project_PC\command_line_app\target\debug\deps\command_line_app.d: src\main.rs
+
+src\main.rs:
