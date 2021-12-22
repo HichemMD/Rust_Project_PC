cd1bf8f0ee13b17606d20fb1c663d14ad2288fc7
Hichem <hichem.djadoudi@gmail.com>
2021-12-22 15:42:36 +0400

    v0.1

--- command_line_app/src/main.rs
+++ command_line_app/src/main.rs
@@ -1,16 +1,39 @@
-use std::io;
+use std::{io, string};
 
 fn main() {
-    println!("Enter your weight (kg): ");
-    let  mut input = String::new();
+    println!("Enter your weight Please: ");
+    Let &mut input = String:: new();
+
+    
+
+
+
 
     io::stdin().read_line(&mut input).unwrap();
 
-    let weight: f32 = input.trim().parse().unwrap();
-    let mars_weight = calculate_weight_on_mars(weight);
-    println!("Weight on Mars: {}kg", mars_weight);
+
 }
 
-fn calculate_weight_on_mars(_weight: f32) -> f32 {
-    (_weight / 9.81) * 3.711
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
