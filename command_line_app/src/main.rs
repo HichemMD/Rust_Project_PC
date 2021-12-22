420af4945c25ef3c19695bd1801725a27fdd6a72
Hichem <hichem.djadoudi@gmail.com>
2021-12-22 15:46:19 +0400

    v1.1

--- command_line_app/src/main.rs
+++ command_line_app/src/main.rs
@@ -1,15 +1,21 @@
 use std::{io, string};
 
 fn main() {
-    println!("Enter your weight Please: ");
-    Let &mut input = String:: new();
-
     
+    println!("Enter your weight Please: ");
+    Let mut input = String:: new();
+
+
 
 
 
 
     io::stdin().read_line(&mut input).unwrap();
+    let weigth: f32= input.trim().purse().unwrap;
+
+
+
+
 
 
 }
