0a05ba7bd5a90e26b124914423984d2a1721f1a9
Hichem <hichem.djadoudi@gmail.com>
2021-12-22 09:55:31 +0400

    init

--- command_line_app/src/main.rs
+++ command_line_app/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     println!("Enter your weight (kg): ");
-    let  input = String::new();
+    let  mut input = String::new();
 
     io::stdin().read_line(&mut input).unwrap();
 
