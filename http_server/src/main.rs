a9cf53ba35fa23de967d2ec40a0d2804145e463a
Hichem <hichem.djadoudi@gmail.com>
2021-12-27 18:04:44 +0400

    V0.0.2

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 
 }
@@ -13,6 +13,6 @@ impl Server {
         }
     }
     fn run(self){
-
+        println!("Listening on {}", self.addr)
     }
 }
---
+++ http_server/target/debug/deps/http_server.d
@@ -0,0 +1,5 @@
+C:\Users\djado\Desktop\Rust_Project_PC\http_server\target\debug\deps\http_server.exe: src\main.rs
+
+C:\Users\djado\Desktop\Rust_Project_PC\http_server\target\debug\deps\http_server.d: src\main.rs
+
+src\main.rs:
