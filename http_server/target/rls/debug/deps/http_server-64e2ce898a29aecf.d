9c4e50f734c1ef70a2dfb483f2128f257bd96523
Hichem <hichem.djadoudi@gmail.com>
2021-12-27 17:41:48 +0400

    V0.01

---
+++ http_server/target/rls/debug/deps/http_server-64e2ce898a29aecf.d
@@ -0,0 +1,5 @@
+c:\Users\djado\Desktop\Rust_Project_PC\http_server\target\rls\debug\deps\http_server-64e2ce898a29aecf.rmeta: src\main.rs
+
+c:\Users\djado\Desktop\Rust_Project_PC\http_server\target\rls\debug\deps\http_server-64e2ce898a29aecf.d: src\main.rs
+
+src\main.rs:
---
+++ http_server/target/rls/debug/deps/http_server-66b037f9515cf103.d
@@ -0,0 +1,5 @@
+c:\Users\djado\Desktop\Rust_Project_PC\http_server\target\rls\debug\deps\http_server-66b037f9515cf103.rmeta: src\main.rs
+
+c:\Users\djado\Desktop\Rust_Project_PC\http_server\target\rls\debug\deps\http_server-66b037f9515cf103.d: src\main.rs
+
+src\main.rs:
